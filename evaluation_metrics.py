187a0dcbd51ac79a6ee5fa3e910a6908979d9d9d
Samuel Yusuf <samuelyusuf@microsoft.com>
2022-02-25 16:11:39 -0500

    progress in evaluation metrics chapter

--- evaluation_metrics.py
+++ evaluation_metrics.py
@@ -1,3 +1,6 @@
+import numpy as np
+from collections import Counter
+
 def true_positive(y_true, y_pred):
     """
     Function to calculate True Positives
@@ -101,3 +104,165 @@ def recall(y_true, y_pred):
     recall = tp / (tp + fn)
 
     return recall
+
+def tpr(y_true, y_pred):
+    """
+    Function to calculate tpr
+    :param y_true: list of true values
+    :param y_pred: list of predicted values
+    :return: tpr/recall
+    """
+    return recall(y_true, y_pred)
+
+def fpr(y_true, y_pred):
+    """
+    Function to calculate fpr
+    :param y_true: list of true values
+    :param y_pred: list of predicted values
+    :return: fpr
+    """
+    fp = false_positive(y_true, y_pred)
+    tn = true_negative(y_true, y_pred)
+    return fp / (tn + fp)
+
+def log_loss(y_true, y_proba):
+    """
+    Function to calculate log loss
+    :param y_true: list of true values
+    :param y_probas: list of probabilities of 1
+    :return: overall log loss
+    """
+    # define an epsilon value
+    # this ca also be an input
+    # this value is used to clip probabilities
+    epsilon = 1e-15
+    # initialize empty list to store
+    # individual losses
+    loss = []
+    # loop over all true and predicted probability values
+    for yt, yp in zip(y_true, y_proba):
+        #adjust probability
+        # 0 gets converted to 1e-15
+        # 1 gets converted to 1-1e-15
+        # Why? Think about what happens when taking log of 0 or 1
+
+        yp = np.clip(yp, epsilon, 1 - epsilon)
+        # calculate loss for one sample
+        temp_loss = -1.0 * (yt * np.log(yp) + (1 - yt) * np.log(1 - yp) )
+        # add to loss list
+        loss.append(temp_loss)
+    #return mean loss of all samples
+    return np.mean(loss)
+
+def macro_precision(y_true, y_pred):
+    """
+    Function to calculate macro averaged precision
+    :param y_true: list of true values
+    :param y_pred: list of predicted values
+    :return: macro precision score
+    """
+
+    # find the number of classes by taking of unique values in the true list
+    num_classes = len(np.unique(y_true))
+
+    # initilize precision to 0
+    precision = 0
+
+    # loop over all classes
+    for class_ in range(num_classes):
+
+        # all classes except current is considered negative
+        temp_true = [1 if p == class_ else 0 for p in y_true]
+        temp_pred = [1 if p == class_ else 0 for p in y_pred]
+
+        # calculate tp for current class
+        tp = true_positive(temp_true, temp_pred)
+
+        # calculate fp for current class
+        fp = false_positive(temp_true, temp_pred)
+
+        # calculate precision for current class
+        temp_precision = tp / (tp + fp)
+
+        # keep adding precision for all classes
+        precision += temp_precision
+
+    # calculate and return average precision over all classes
+    precision /= num_classes
+    return precision
+
+def micro_precision(y_true, y_pred):
+    """
+    Function to calculate macro averaged precision
+    :param y_true: list of true values
+    :param y_pred: list of predicted values
+    :return: micro precision score
+    """
+
+    # find the number of classes by taking of unique values in the true list
+    num_classes = len(np.unique(y_true))
+
+    # initilize tp and fp to 0
+    tp = 0
+    fp = 0
+
+    # loop over all classes
+    for class_ in range(num_classes):
+
+        # all classes except current is considered negative
+        temp_true = [1 if p == class_ else 0 for p in y_true]
+        temp_pred = [1 if p == class_ else 0 for p in y_pred]
+
+        # calculate tp for current class and update overall tp
+        tp += true_positive(temp_true, temp_pred)
+
+        # calculate fp for current class and update overall fp
+        fp += false_positive(temp_true, temp_pred)
+
+        # calculate precision for current class
+        temp_precision = tp / (tp + fp)
+
+        # keep adding precision for all classes
+        precision += temp_precision
+
+    # calculate and return average precision over all classes
+    precision /= num_classes
+    return precision
+
+def weighted_precision(y_true, y_pred):
+    """
+    Function to calculate weighted averaged precision
+    :param y_true: list of true values
+    :param y_pred: list of predicted values
+    :return: weighted precision score
+    """
+
+    # find the number of classes by taking length of unique values in true list
+    num_classes = len(np.unique(y_true))
+
+    # create class:sample count dictionary
+    class_counts = Counter(y_true)
+
+    # initialize precision to 0
+    precision = 0
+
+    # loop over all classes
+    for class_ in range(num_classes):
+        # all classes except current are considered negative
+        temp_true = [1 if p == class_ else 0 for p in y_true]
+        temp_pred = [1 if p == class_ else 0 for p in y_pred]
+
+        # calculate tp and fp for class
+        tp = true_positive(temp_true, temp_pred)
+        fp = false_positive(temp_true, temp_pred)
+
+        # calculate precision of class
+        temp_precision = tp / (tp + fp)
+
+        # multiply precision with count of samples in class
+        weighted_precision = class_counts[class_] * temp_precision
+
+        precision += weighted_precision
+    # calculate overall precision by dividing by total number of samples
+    overall_precision = precision / len(y_true)
+    return overall_precision
